2d4348071fa7115738f8cb0305fa1bcea023b597
Yogesh Kumar <yksainimbd@gmail.com>
2024-06-25 14:38:20 +0530

    re-coding

--- src/app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='h-full flex flex-col justify-center items-center'>
-      {children}
-    </div>
-  );
-};
-
-export default AuthLayout;
